8b36e04e60e3125b47cee76af583a32235d40839
Yunpeng Li <yli4@umbc.edu>
2022-08-16 18:16:02 -0400

    Added more tests

--- src/test/java/org/dragon/yunpeng/MyFirstSeleniumTest.java
+++ src/test/java/org/dragon/yunpeng/MyFirstSeleniumTest.java
@@ -3,17 +3,41 @@ package org.dragon.yunpeng;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class MyFirstSeleniumTest extends AbstractChromeWebDriverTest {
 
 	@Test
-	public void test() {
+	public void testUMBC() {
 		// get the url
 		driver.get("https://umbc.edu/");
 
 		sleep(5);
 		// assert the title
 		assertEquals("UMBC: University Of Maryland, Baltimore County", driver.getTitle());// First Web Application
+
+	}
+
+	@Test
+	public void testGoogle() {
+		// Get the url
+		driver.get("https://www.google.com/");
+
+		// Find the search input box
+		WebElement searchInput = driver.findElement(By.cssSelector(".gLFyf.gsfi"));
+
+		System.out.println("-------------------------------------------------------");
+		System.out.println(searchInput.getAttribute("title"));
+		System.out.println("-------------------------------------------------------");
+
+		// Enter search text
+		searchInput.sendKeys("Tiger");
+
+		// Submit form
+		searchInput.submit();
+
+		sleep(5);
 
 	}
 
